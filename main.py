3accb9b13afe7eab96bf7d50f7f14bb469e2075c
jbite <jbite9057>
2024-12-18 21:01:36 +0800

    Day 28: Add links, sepcify language, limti news, add subject, control category

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import requests
 from key import key
 import send_email
+from datetime import datetime, timedelta
+
+def get_date_before_days(days):
+  """
+  Calculates the date 'days' before today.
+
+  Args:
+    days: The number of days to go back.
+
+  Returns:
+    A datetime object representing the date 'days' before today.
+  """
+  today = datetime.today()
+  past_date = today - timedelta(days=days)
+ 
+  return past_date
+
 api_Key = key.api_Key
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-11-17&sortBy=publishedAt&apiKey={api_Key}"
+topic = input("Please type the topic you want to receive: ")
+numberNews = int(input("How many news you want to receive?: "))
+fromday = get_date_before_days(30)
+
+url = f"https://newsapi.org/v2/everything?q={topic}&from={fromday}&sortBy=publishedAt&apiKey={api_Key}&language=zh"
 
 #make request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
+body = ""
+# Add news to email
+for article in content['articles'][:numberNews]:
+    if len(article['title']) > 0:
+        body = body + f"Title: {article['title']}\nContent: {article['description']}\nLink: {article['url']}\n==" + 2*"\n"
 
 # Access the article titles and description
-title = []
-for article in content['articles']:
-    title.append(article["title"])
-
+# Send email to yourself
 sender_email = key.email
 sender_password = key.email_pass 
 recipient_email = key.email
-subject = "News for Tesla"
-body = title[0]
+subject = f"News for {topic}"
 
 send_email.send_email(sender_email, sender_password, recipient_email, subject, body)
